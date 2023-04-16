ea869cf70bc68b777477ee11e5b20001b08b8a10
claudia.fernandezz <claudia.fernandezz@estudiante.uam>
2023-04-16 12:37:14 +0200

    Llamada conseguida (problemitas en permisos Manifest)

--- app/src/main/java/com/example/mysecurity/AyudaFragment.kt
+++ app/src/main/java/com/example/mysecurity/AyudaFragment.kt
@@ -1,23 +1,24 @@
 package com.example.mysecurity
 
-import android.Manifest.permission.CALL_PHONE
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.mysecurity.databinding.FragmentAyudaBinding
 
+
 class AyudaFragment : Fragment() {
     lateinit var binding: FragmentAyudaBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,22 +34,19 @@ class AyudaFragment : Fragment() {
             view.findNavController().navigate(R.id.action_ayudaFragment_to_principalFragment)
         }
 
-        Thread.sleep(5_000)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            when {
-                ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.CALL_PHONE
-                ) == PackageManager.PERMISSION_GRANTED ->{
-                    startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:637093883")))
+        //Thread.sleep(5_000)
+        //startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:637093883")))
 
-                }
-            }
+
+        val permission = ContextCompat.checkSelfPermission(requireContext().applicationContext,
+            Manifest.permission.CALL_PHONE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // ACTION_CALL
+            startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:637093883")))
 
         }
-        else{
-            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:637093883")))
-        }
+
 
 
         return binding.root
